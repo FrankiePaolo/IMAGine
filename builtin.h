c9622870f3948b46a49f4b327b6982b87228d461
Frank <francescopaolo.castiglione@community.unipa.it>
2020-09-04 16:40:38 +0200

    Signatures updated
    builtin.h

--- builtin.h
+++ builtin.h
@@ -5,7 +5,15 @@ struct utils * callbuiltin(struct fncall * f);
 
 /* built-in functions */
 void print_B(struct utils * v);
+
+/* methods for images */
 void getWidth(struct symref * v);
 void invert(struct symref * v);
 void average(struct symref * v);
+void openImg(char * path);
+
+/* methods for lists */
 struct utils * depth(struct symbol * e);
+void get(struct symbol * e,struct utils * v);
+void push(struct symbol * e,struct utils * v);
+void pop(struct symbol * e);
