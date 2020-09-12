03d0328242db09e3c9fdd53acb28d158f69cb160
Frank <francescopaolo.castiglione@community.unipa.it>
2020-09-12 15:26:12 +0200

    ArgumentsCheck pop(5);

--- utils.c
+++ utils.c
@@ -69,10 +69,21 @@ argumentsCheck(struct fncall * f, int index){
    struct ast * node=f->l;
    int counter=1;
 
+   if(type(node) =='i' || type(node) =='D'){
+      yyerror("The argument provided is not a list!");
+      exit(0);
+   }else if(node->r && node->nodetype=='L'){
+      do{
+         node=node->r;
+         counter++;
+      }while(node->r && node->nodetype=='L');
+   }
+
+   /*
    while(node->r && node->nodetype=='L'){
       node=node->r;
       counter++;
-   }
+   }*/
 
    if(counter<index){
       yyerror("Too few arguments for the function! Requested: %d but given: %d!", index, counter);
