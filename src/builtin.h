5d8bde3a06aa67ddbda40b9ed15d4c1b2fbed19a
Cuchini <davide.iraci@community.unipa.it>
2020-09-14 13:52:34 -0400

    Indention

--- src/builtin.h
+++ src/builtin.h
@@ -1,13 +1,13 @@
 /* call built in functions */
-struct utils * callbuiltin(struct fncall * f);
+    struct utils * callbuiltin(struct fncall * f);
 
 /* built-in functions */
-void print_B(struct utils * v);
+    void print_B(struct utils * v);
 
 /* methods for lists */
-struct utils * length(struct symref * e);
-struct utils * get(struct symref * e,struct utils * v);
-void push(struct symref * e,struct utils * v);
-void list_remove(struct symref * e, struct utils * s);
-void pop(struct symref * e);
-void insert(struct symref * e, struct utils * v, struct utils * s);
+    struct utils * length(struct symref * e);
+    struct utils * get(struct symref * e,struct utils * v);
+    void push(struct symref * e,struct utils * v);
+    void list_remove(struct symref * e, struct utils * s);
+    void pop(struct symref * e);
+    void insert(struct symref * e, struct utils * v, struct utils * s);
--- src/operations.c
+++ src/operations.c
@@ -1,33 +1,33 @@
-#  include <stdio.h>
-#  include <stdlib.h>
-#  include <stdarg.h>
-#  include <string.h>
-#  include <math.h>
-#  include <vips/vips.h>
-#  include "utils.h"
-#  include "builtin.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <math.h>
+#include <vips/vips.h>
+#include "utils.h"
+#include "builtin.h"
 
 void
 sum(struct utils * v, struct utils * l, struct utils * r) {
 
    if (type(l) == 'i' && type(r) == 'i') {
-      putElement_i(v,getElement_i(l)+getElement_i(r));
+      putElement_i(v, getElement_i(l) + getElement_i(r));
    } else if (type(l) == 'D' && type(r) == 'D') {
-      putElement_d(v,getElement_d(l)+getElement_d(r));
+      putElement_d(v, getElement_d(l) + getElement_d(r));
    } else if (type(l) == 'i' && type(r) == 'D') {
-      putElement_d(v,getElement_i(l)+getElement_d(r));
+      putElement_d(v, getElement_i(l) + getElement_d(r));
    } else if (type(l) == 'D' && type(r) == 'i') {
       sum(v, r, l);
-   } else if ((type(l) == 'S' && type(r) == 'i')) { 
-      asprintf( &(((struct str *)v)->str), "%s%d", getElement_s(l), getElement_i(r) );
-   } else if ((type(l) == 'i' && type(r) == 'S')) { 
-      asprintf( &(((struct str *)v)->str), "%d%s", getElement_i(l), getElement_s(r) );
-   } else if ((type(l) == 'S' && type(r) == 'D')) { 
-      asprintf( &(((struct str *)v)->str), "%s%.6g", getElement_s(l), getElement_d(r) );
-   } else if ((type(l) == 'D' && type(r) == 'S')) { 
-      asprintf( &(((struct str *)v)->str), "%.6g%s", getElement_d(l), getElement_s(r) );
-   } else if ((type(l) == 'S' && type(r) == 'S')) { 
-      asprintf( &(((struct str *)v)->str), "%s%s", getElement_s(l), getElement_s(r) );
+   } else if ((type(l) == 'S' && type(r) == 'i')) {
+      asprintf( & (((struct str * ) v) -> str), "%s%d", getElement_s(l), getElement_i(r));
+   } else if ((type(l) == 'i' && type(r) == 'S')) {
+      asprintf( & (((struct str * ) v) -> str), "%d%s", getElement_i(l), getElement_s(r));
+   } else if ((type(l) == 'S' && type(r) == 'D')) {
+      asprintf( & (((struct str * ) v) -> str), "%s%.6g", getElement_s(l), getElement_d(r));
+   } else if ((type(l) == 'D' && type(r) == 'S')) {
+      asprintf( & (((struct str * ) v) -> str), "%.6g%s", getElement_d(l), getElement_s(r));
+   } else if ((type(l) == 'S' && type(r) == 'S')) {
+      asprintf( & (((struct str * ) v) -> str), "%s%s", getElement_s(l), getElement_s(r));
    } else if (type(l) == 'N' && type(r) != 'N') {
       sum(v, getElement_sym(l), r);
    } else if (type(l) != 'N' && type(r) == 'N') {
@@ -43,13 +43,13 @@ void
 subtract(struct utils * v, struct utils * l, struct utils * r) {
 
    if (type(l) == 'i' && type(r) == 'i') {
-      putElement_i(v,getElement_i(l)-getElement_i(r));
+      putElement_i(v, getElement_i(l) - getElement_i(r));
    } else if (type(l) == 'D' && type(r) == 'D') {
-      putElement_d(v,getElement_d(l)-getElement_d(r));
+      putElement_d(v, getElement_d(l) - getElement_d(r));
    } else if (type(l) == 'i' && type(r) == 'D') {
-      putElement_d(v,getElement_i(l)-getElement_d(r));
+      putElement_d(v, getElement_i(l) - getElement_d(r));
    } else if (type(l) == 'D' && type(r) == 'i') {
-      putElement_d(v,getElement_d(l)-getElement_i(r));
+      putElement_d(v, getElement_d(l) - getElement_i(r));
    } else if (type(l) == 'N' && type(r) != 'N') {
       subtract(v, getElement_sym(l), r);
    } else if (type(l) != 'N' && type(r) == 'N') {
@@ -65,24 +65,24 @@ void
 multiply(struct utils * v, struct utils * l, struct utils * r) {
 
    if (type(l) == 'i' && type(r) == 'i') {
-      putElement_i(v,getElement_i(l)*getElement_i(r));
+      putElement_i(v, getElement_i(l) * getElement_i(r));
    } else if (type(l) == 'D' && type(r) == 'D') {
-      putElement_d(v,getElement_d(l)*getElement_d(r));
+      putElement_d(v, getElement_d(l) * getElement_d(r));
    } else if (type(l) == 'i' && type(r) == 'D') {
-      putElement_d(v,getElement_i(l)*getElement_d(r));
+      putElement_d(v, getElement_i(l) * getElement_d(r));
    } else if (type(l) == 'D' && type(r) == 'i') {
       multiply(v, r, l);
    } else if ((type(l) == 'S' && type(r) == 'i')) {
-      if(getElement_i(r) >0 ){
-         asprintf( &(((struct str * ) v)->str), "%s", getElement_s(l));
-         for(int i=0; i<getElement_i(r); i++){
-            asprintf(&(((struct str * ) v)->str), "%s%s", getElement_s(v), getElement_s(l));
+      if (getElement_i(r) > 0) {
+         asprintf( & (((struct str * ) v) -> str), "%s", getElement_s(l));
+         for (int i = 0; i < getElement_i(r); i++) {
+            asprintf( & (((struct str * ) v) -> str), "%s%s", getElement_s(v), getElement_s(l));
          }
-      }else{
+      } else {
          yyerror("String multiply allowed only for positive integer");
-      }       
+      }
    } else if ((type(l) == 'i' && type(r) == 'S')) {
-      multiply(v, r , l); 
+      multiply(v, r, l);
    } else if (type(l) == 'N' && type(r) != 'N') {
       multiply(v, getElement_sym(l), r);
    } else if (type(l) != 'N' && type(r) == 'N') {
@@ -95,47 +95,47 @@ multiply(struct utils * v, struct utils * l, struct utils * r) {
 }
 
 struct utils *
-divide(struct utils * l, struct utils * r) {
-   struct utils * v;
-   v=malloc(sizeof(struct doublePrecision));
-   ((struct doublePrecision * ) v) -> nodetype = 'D';
-   
-   if (type(l) == 'i' && type(r) == 'D') {
-      if(getElement_d(r) !=0){
-         putElement_d(v,getElement_i(l) / getElement_d(r));
+   divide(struct utils * l, struct utils * r) {
+      struct utils * v;
+      v = malloc(sizeof(struct doublePrecision));
+      ((struct doublePrecision * ) v) -> nodetype = 'D';
+
+      if (type(l) == 'i' && type(r) == 'D') {
+         if (getElement_d(r) != 0) {
+            putElement_d(v, getElement_i(l) / getElement_d(r));
+         }
+      } else if (type(l) == 'D' && type(r) == 'i') {
+         if (getElement_i(r) != 0) {
+            putElement_d(v, getElement_d(l) / getElement_i(r));
+         }
+      } else if (type(l) == 'i' && type(r) == 'i') {
+         if (getElement_i(r) != 0) {
+            putElement_d(v, (double)(getElement_i(l)) / (double)(getElement_i(r)));
+         }
+      } else if (type(l) == 'D' && type(r) == 'D') {
+         if (getElement_d(r) != 0) {
+            putElement_d(v, getElement_d(l) / getElement_d(r));
+         }
+      } else if (type(l) == 'N' && type(r) != 'N') {
+         v = divide(getElement_sym(l), r);
+      } else if (type(l) != 'N' && type(r) == 'N') {
+         v = divide(l, getElement_sym(r));
+      } else if (type(l) == 'N' && type(r) == 'N') {
+         v = divide(getElement_sym(l), getElement_sym(r));
+      } else {
+         yyerror("Cannot perform the division operation on given values");
       }
-   } else if (type(l) == 'D' && type(r) == 'i') {
-      if(getElement_i(r) !=0){
-         putElement_d(v,getElement_d(l) / getElement_i(r));
-      }
-   } else if (type(l) == 'i' && type(r) == 'i') {
-      if(getElement_i(r) !=0){
-         putElement_d(v,(double) (getElement_i(l)) / (double)(getElement_i(r)));
-      }
-   } else if (type(l) == 'D' && type(r) == 'D') {
-      if(getElement_d(r) !=0){
-         putElement_d(v,getElement_d(l) / getElement_d(r));
-      }
-   } else if (type(l) == 'N' && type(r) != 'N') {
-      v=divide(getElement_sym(l), r);
-   } else if (type(l) != 'N' && type(r) == 'N') {
-      v=divide(l, getElement_sym(r));
-   } else if (type(l) == 'N' && type(r) == 'N') {
-      v=divide(getElement_sym(l), getElement_sym(r));
-   } else {
-      yyerror("Cannot perform the division operation on given values");
+      return v;
    }
-   return v;
-}
 
 void
 absoluteValue(struct utils * v, struct utils * l) {
    if (type(l) == 'N') {
       absoluteValue(v, getElement_sym(l));
    } else if (type(v) == 'i') {
-      putElement_i(v,abs(getElement_i(l)));
+      putElement_i(v, abs(getElement_i(l)));
    } else if (type(v) == 'D') {
-      putElement_d(v,fabs(getElement_d(l)));
+      putElement_d(v, fabs(getElement_d(l)));
    } else {
       yyerror("Cannot perform the absolute value operation on given values");
    }
@@ -145,20 +145,20 @@ void
 biggerThan(struct utils * v, struct utils * l, struct utils * r) {
 
    if (type(l) == 'N' && type(r) != 'N') {
-      biggerThan( v, getElement_sym(l), r);
+      biggerThan(v, getElement_sym(l), r);
    } else if (type(l) != 'N' && type(r) == 'N') {
-      biggerThan( v, l, getElement_sym(r));
+      biggerThan(v, l, getElement_sym(r));
    } else if (type(l) == 'N' && type(r) == 'N') {
-      biggerThan( v, getElement_sym(l), getElement_sym(r));
+      biggerThan(v, getElement_sym(l), getElement_sym(r));
    } else if (type(l) == 'i' && type(r) == 'D') {
-      putElement_i(v,getElement_i(l) > getElement_d(r) ? 1 : 0);
+      putElement_i(v, getElement_i(l) > getElement_d(r) ? 1 : 0);
    } else if (type(l) == 'D' && type(r) == 'i') {
-      putElement_i(v,getElement_d(l) > getElement_i(r) ? 1 : 0);
-   } else if (type(l) == 'i' && type(r) == 'i'){
-      putElement_i(v,getElement_i(l) > getElement_i(r) ? 1 : 0);
-   } else if(type(l) == 'D' && type(r) == 'D'){
-      putElement_i(v,getElement_d(l) > getElement_d(r) ? 1 : 0);
-   }else {
+      putElement_i(v, getElement_d(l) > getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'i' && type(r) == 'i') {
+      putElement_i(v, getElement_i(l) > getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'D' && type(r) == 'D') {
+      putElement_i(v, getElement_d(l) > getElement_d(r) ? 1 : 0);
+   } else {
       yyerror("Cannot perform the comparison operation (>) on given values");
    }
 }
@@ -167,20 +167,20 @@ void
 smallerThan(struct utils * v, struct utils * l, struct utils * r) {
 
    if (type(l) == 'N' && type(r) != 'N') {
-      smallerThan( v, getElement_sym(l), r);
+      smallerThan(v, getElement_sym(l), r);
    } else if (type(l) != 'N' && type(r) == 'N') {
-      smallerThan( v, l, getElement_sym(r));
+      smallerThan(v, l, getElement_sym(r));
    } else if (type(l) == 'N' && type(r) == 'N') {
-      smallerThan( v, getElement_sym(l), getElement_sym(r));
-   }else if (type(l) == 'i' && type(r) == 'D') {
-      putElement_i(v,getElement_i(l) < getElement_d(r) ? 1 : 0);
-   }else if (type(l) == 'D' && type(r) == 'i') {
-      putElement_i(v,getElement_d(l) < getElement_i(r) ? 1 : 0);
-   }else if (type(l) == 'i' && type(r) == 'i'){
-      putElement_i(v,getElement_i(l) < getElement_i(r) ? 1 : 0);
-   }else if(type(l) == 'D' && type(r) == 'D'){
-      putElement_i(v,getElement_d(l) < getElement_d(r) ? 1 : 0);
-   }else {
+      smallerThan(v, getElement_sym(l), getElement_sym(r));
+   } else if (type(l) == 'i' && type(r) == 'D') {
+      putElement_i(v, getElement_i(l) < getElement_d(r) ? 1 : 0);
+   } else if (type(l) == 'D' && type(r) == 'i') {
+      putElement_i(v, getElement_d(l) < getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'i' && type(r) == 'i') {
+      putElement_i(v, getElement_i(l) < getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'D' && type(r) == 'D') {
+      putElement_i(v, getElement_d(l) < getElement_d(r) ? 1 : 0);
+   } else {
       yyerror("Cannot perform the comparison operation (<) on given values");
    }
 }
@@ -189,98 +189,98 @@ void
 unequal(struct utils * v, struct utils * l, struct utils * r) {
 
    if (type(l) == 'i' && type(r) == 'D') {
-      putElement_i(v,getElement_i(l) != getElement_d(r) ? 1 : 0);
-   }else if (type(l) == 'D' && type(r) == 'i') {
-      putElement_i(v,getElement_d(l) != getElement_i(r) ? 1 : 0);
-   }else if (type(l) == 'i' && type(r) == 'i'){
-      putElement_i(v,getElement_i(l) != getElement_i(r) ? 1 : 0);
-   }else if(type(l) == 'D' && type(r) == 'D'){
-      putElement_i(v,getElement_d(l) != getElement_d(r) ? 1 : 0);
-   }else if (type(l) == 'S' && type(r) == 'S'){
+      putElement_i(v, getElement_i(l) != getElement_d(r) ? 1 : 0);
+   } else if (type(l) == 'D' && type(r) == 'i') {
+      putElement_i(v, getElement_d(l) != getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'i' && type(r) == 'i') {
+      putElement_i(v, getElement_i(l) != getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'D' && type(r) == 'D') {
+      putElement_i(v, getElement_d(l) != getElement_d(r) ? 1 : 0);
+   } else if (type(l) == 'S' && type(r) == 'S') {
       int temp;
-      temp = strcmp(strdup(getElement_s(l)),strdup(getElement_s(r)));
-      if(temp!=0){
-         temp=1;
+      temp = strcmp(strdup(getElement_s(l)), strdup(getElement_s(r)));
+      if (temp != 0) {
+         temp = 1;
       }
-      putElement_i(v,temp);
-   }else if (type(l) == 'N' && type(r) != 'N') {
-      unequal( v, getElement_sym(l), r);
+      putElement_i(v, temp);
+   } else if (type(l) == 'N' && type(r) != 'N') {
+      unequal(v, getElement_sym(l), r);
    } else if (type(l) != 'N' && type(r) == 'N') {
-      unequal( v, l, getElement_sym(r));
+      unequal(v, l, getElement_sym(r));
    } else if (type(l) == 'N' && type(r) == 'N') {
-      unequal( v, getElement_sym(l), getElement_sym(r));
-   }else {
+      unequal(v, getElement_sym(l), getElement_sym(r));
+   } else {
       yyerror("Cannot perform the comparison operation (!=) on given values");
    }
 }
 
 void
 equal(struct utils * v, struct utils * l, struct utils * r) {
-   
+
    if (type(l) == 'i' && type(r) == 'D') {
-      putElement_i(v,getElement_i(l) == getElement_d(r) ? 1 : 0);
-   }else if (type(l) == 'D' && type(r) == 'i') {
-      putElement_i(v,getElement_d(l) == getElement_i(r) ? 1 : 0);
-   }else if (type(l) == 'i' && type(r) == 'i'){
-      putElement_i(v,getElement_i(l) == getElement_i(r) ? 1 : 0);
-   }else if(type(l) == 'D' && type(r) == 'D'){
-      putElement_i(v,getElement_d(l) == getElement_d(r) ? 1 : 0);
-   }else if (type(l) == 'S' && type(r) == 'S'){
+      putElement_i(v, getElement_i(l) == getElement_d(r) ? 1 : 0);
+   } else if (type(l) == 'D' && type(r) == 'i') {
+      putElement_i(v, getElement_d(l) == getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'i' && type(r) == 'i') {
+      putElement_i(v, getElement_i(l) == getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'D' && type(r) == 'D') {
+      putElement_i(v, getElement_d(l) == getElement_d(r) ? 1 : 0);
+   } else if (type(l) == 'S' && type(r) == 'S') {
       int temp;
-      temp = strcmp(strdup(getElement_s(l)),strdup(getElement_s(r)));
-      temp=getTruth(temp);
-      putElement_i(v,temp);
-   }else if (type(l) == 'N' && type(r) != 'N') {
-      equal( v, getElement_sym(l), r);
+      temp = strcmp(strdup(getElement_s(l)), strdup(getElement_s(r)));
+      temp = getTruth(temp);
+      putElement_i(v, temp);
+   } else if (type(l) == 'N' && type(r) != 'N') {
+      equal(v, getElement_sym(l), r);
    } else if (type(l) != 'N' && type(r) == 'N') {
-      equal( v, l, getElement_sym(r));
+      equal(v, l, getElement_sym(r));
    } else if (type(l) == 'N' && type(r) == 'N') {
-      equal( v, getElement_sym(l), getElement_sym(r));
-   }else {
+      equal(v, getElement_sym(l), getElement_sym(r));
+   } else {
       yyerror("Cannot perform the comparison operation (==) on given values");
    }
 }
 
 void
 biggerOrEqual(struct utils * v, struct utils * l, struct utils * r) {
-   
+
    if (type(l) == 'i' && type(r) == 'D') {
-      putElement_i(v,getElement_i(l) >= getElement_d(r) ? 1 : 0);
-   }else if (type(l) == 'D' && type(r) == 'i') {
-      putElement_i(v,getElement_d(l) >= getElement_i(r) ? 1 : 0);
-   }else if (type(l) == 'i' && type(r) == 'i'){
-      putElement_i(v,getElement_i(l) >= getElement_i(r) ? 1 : 0);
-   }else if(type(l) == 'D' && type(r) == 'D'){
-      putElement_i(v,getElement_d(l) >= getElement_d(r) ? 1 : 0);
-   }else if (type(l) == 'N' && type(r) != 'N') {
-      biggerOrEqual( v, getElement_sym(l), r);
+      putElement_i(v, getElement_i(l) >= getElement_d(r) ? 1 : 0);
+   } else if (type(l) == 'D' && type(r) == 'i') {
+      putElement_i(v, getElement_d(l) >= getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'i' && type(r) == 'i') {
+      putElement_i(v, getElement_i(l) >= getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'D' && type(r) == 'D') {
+      putElement_i(v, getElement_d(l) >= getElement_d(r) ? 1 : 0);
+   } else if (type(l) == 'N' && type(r) != 'N') {
+      biggerOrEqual(v, getElement_sym(l), r);
    } else if (type(l) != 'N' && type(r) == 'N') {
-      biggerOrEqual( v, l, getElement_sym(r));
+      biggerOrEqual(v, l, getElement_sym(r));
    } else if (type(l) == 'N' && type(r) == 'N') {
-      biggerOrEqual( v, getElement_sym(l), getElement_sym(r));
-   }else {
+      biggerOrEqual(v, getElement_sym(l), getElement_sym(r));
+   } else {
       yyerror("Cannot perform the comparison operation (>=) on given values");
    }
 }
 
 void
 smallerOrEqual(struct utils * v, struct utils * l, struct utils * r) {
-   
+
    if (type(l) == 'i' && type(r) == 'D') {
-      putElement_i(v,getElement_i(l) <= getElement_d(r) ? 1 : 0);
-   }else if (type(l) == 'D' && type(r) == 'i') {
-      putElement_i(v,getElement_d(l) <= getElement_i(r) ? 1 : 0);
-   }else if (type(l) == 'i' && type(r) == 'i'){
-      putElement_i(v,getElement_i(l) <= getElement_i(r) ? 1 : 0);
-   }else if(type(l) == 'D' && type(r) == 'D'){
-      putElement_i(v,getElement_d(l) <= getElement_d(r) ? 1 : 0);
-   }else if (type(l) == 'N' && type(r) != 'N') {
-      smallerOrEqual( v, getElement_sym(l), r);
+      putElement_i(v, getElement_i(l) <= getElement_d(r) ? 1 : 0);
+   } else if (type(l) == 'D' && type(r) == 'i') {
+      putElement_i(v, getElement_d(l) <= getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'i' && type(r) == 'i') {
+      putElement_i(v, getElement_i(l) <= getElement_i(r) ? 1 : 0);
+   } else if (type(l) == 'D' && type(r) == 'D') {
+      putElement_i(v, getElement_d(l) <= getElement_d(r) ? 1 : 0);
+   } else if (type(l) == 'N' && type(r) != 'N') {
+      smallerOrEqual(v, getElement_sym(l), r);
    } else if (type(l) != 'N' && type(r) == 'N') {
-      smallerOrEqual( v, l, getElement_sym(r));
+      smallerOrEqual(v, l, getElement_sym(r));
    } else if (type(l) == 'N' && type(r) == 'N') {
-      smallerOrEqual( v, getElement_sym(l), getElement_sym(r));
-   }else {
+      smallerOrEqual(v, getElement_sym(l), getElement_sym(r));
+   } else {
       yyerror("Cannot perform the comparison operation (<=) on given values");
    }
 }
